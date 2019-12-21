8f64fc7fe550ee43b33e4940cd9b9dae7f45f753
Bagus Aditama <aditoye105@gmail.com>
2019-12-21 08:41:36 +0700

    fungsi login

--- src/MainAdmin.java
+++ src/MainAdmin.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.sql.*;
 
-/**
- *
- * @author aditjoos
- */
 public class MainAdmin extends javax.swing.JFrame {
 
-    /**
-     * Creates new form MainAdmin
-     */
+    // deklarasi
+    Connection con;
+    Statement stat;
+    ResultSet rs;
+    String sql;
+    
     public MainAdmin() {
         initComponents();
     }
